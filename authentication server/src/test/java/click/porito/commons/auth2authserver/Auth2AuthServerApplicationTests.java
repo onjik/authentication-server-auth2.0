2cd11840815cbf06a271e9cee138fb2a60926153
Onji Kim <xodks672@gmail.com>
2023-07-29 20:41:35 +0900

    [UPDATE] spring boot test base profile

--- authentication server/src/test/java/click/porito/commons/auth2authserver/Auth2AuthServerApplicationTests.java
+++ authentication server/src/test/java/click/porito/commons/auth2authserver/Auth2AuthServerApplicationTests.java
@@ -2,7 +2,9 @@ package click.porito.commons.auth2authserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class Auth2AuthServerApplicationTests {
 
