610732ba1c40f633566f14fde34b76739cc1ee5d
Onji Kim <xodks672@gmail.com>
2023-08-04 21:11:30 +0900

    [UPDATE] database design
    - 유저 관련 보안 정보(lock, disable, expired) 설계 반영
    - 토큰 설계 변경(한개의 토큰 종류는 한개만 가질 수 있도록 강제)

--- authorization database/database.sql
+++ authorization database/database.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE auth2_authorization_server;
+
 /*
  static table
  초기에 값을 박아놓고 시작 하는 테이블
@@ -65,12 +65,17 @@ CREATE TABLE redirect_uri
 
 CREATE TABLE resource_owner
 (
-    id     varchar(50) PRIMARY KEY,
-    name   varchar(255) NOT NULL,
-    gender char(1)      NOT NULL,
-    email  varchar(255) NOT NULL,
+    id          varchar(50) PRIMARY KEY,
+    name        varchar(255) NOT NULL,
+    gender      char(1)      NOT NULL,
+    email       varchar(255) NOT NULL,
+    created_at  timestamp DEFAULT NOW(),
+    expires_at  timestamp DEFAULT NULL,
+    is_locked   bool      DEFAULT FALSE,
+    is_disabled bool      DEFAULT FALSE,
     CONSTRAINT unique_email UNIQUE (email)
 );
+
 
 CREATE TABLE credential
 (
@@ -83,8 +88,11 @@ CREATE TABLE credential
 CREATE TABLE password
 (
     credential_id bigint PRIMARY KEY REFERENCES credential (id),
-    value         varchar(255) NOT NULL
+    value         varchar(255) NOT NULL,
+    issued_at     timestamp DEFAULT NOW(),
+    expires_at    timestamp DEFAULT NULL
 );
+
 
 
 CREATE TABLE authorization_consent
@@ -96,48 +104,63 @@ CREATE TABLE authorization_consent
 );
 
 
+
+CREATE TABLE access_token
+(
+    id         bigserial PRIMARY KEY,
+    token_type varchar(255) NOT NULL,
+    metadata   json      DEFAULT NULL,
+    value      text         NOT NULL,
+    issued_at  timestamp DEFAULT NOW(),
+    expired_at timestamp    NOT NULL
+);
+
+
+
+CREATE TABLE refresh_token
+(
+    id         bigserial PRIMARY KEY,
+    metadata   json      DEFAULT NULL,
+    value      text NOT NULL,
+    issued_at  timestamp DEFAULT NOW(),
+    expired_at timestamp DEFAULT NULL
+);
+
+
+CREATE TABLE authorization_code
+(
+    id         bigserial PRIMARY KEY,
+    metadata   json      DEFAULT NULL,
+    value      text NOT NULL,
+    issued_at  timestamp DEFAULT NOW(),
+    expired_at timestamp DEFAULT NULL
+);
+
+
+CREATE TABLE oidc_id_token
+(
+    id         bigserial PRIMARY KEY,
+    claims     json NOT NULL,
+    metadata   json      DEFAULT NULL,
+    value      text NOT NULL,
+    issued_at  timestamp DEFAULT NOW(),
+    expired_at timestamp DEFAULT NULL
+);
+
 CREATE TABLE oauth2_authorization
 (
     id                          bigserial PRIMARY KEY,
     resource_owner_id           varchar(50) NOT NULL REFERENCES resource_owner (id),
     authorization_grant_type_id bigint      NOT NULL REFERENCES authorization_grant_type (id),
     attribute                   json         DEFAULT NULL,
-    state                       varchar(500) DEFAULT NULL
+    state                       varchar(500) DEFAULT NULL,
+    access_token_id             bigint       DEFAULT NULL REFERENCES access_token (id),
+    refresh_token_id            bigint       DEFAULT NULL REFERENCES refresh_token (id),
+    authorization_code_id       bigint       DEFAULT NULL REFERENCES authorization_code (id),
+    oidc_token_id               bigint       DEFAULT NULL REFERENCES oidc_id_token (id)
 );
 
 
-CREATE TABLE token
-(
-    id               bigserial PRIMARY KEY,
-    oauth2_authorization_id bigint    NOT NULL REFERENCES oauth2_authorization (id),
-    metadata         json      DEFAULT NULL,
-    value            text      NOT NULL,
-    issued_at        timestamp DEFAULT NOW(),
-    expired_at       timestamp DEFAULT NULL,
-    token_type       varchar(255)
-);
-
-CREATE TABLE access_token
-(
-    token_id   bigint PRIMARY KEY REFERENCES token (id),
-    token_type varchar(255) NOT NULL
-);
-
-CREATE TABLE refresh_token
-(
-    token_id bigint PRIMARY KEY REFERENCES token (id)
-);
-
-CREATE TABLE authorization_code
-(
-    token_id bigint PRIMARY KEY REFERENCES token (id)
-);
-
-CREATE TABLE oidc_id_token
-(
-    token_id bigint PRIMARY KEY REFERENCES token (id),
-    claims   json NOT NULL
-);
 
 /*
  mapping tables
@@ -161,7 +184,7 @@ CREATE TABLE authorization_consent_role
 CREATE TABLE access_token_scope
 (
     scope_id bigint REFERENCES scope (id),
-    token_id bigint REFERENCES access_token (token_id),
+    token_id bigint REFERENCES access_token (id),
     PRIMARY KEY (scope_id,token_id)
 );
 
@@ -199,14 +222,3 @@ CREATE TABLE client_authorization_grant_type
     authorization_grant_type_id bigint REFERENCES authorization_grant_type (id),
     PRIMARY KEY (client_id, authorization_grant_type_id)
 );
-
-
-
-
-
-
-
-
-
-
-
