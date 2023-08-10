9352d3064eefc9547c60d27d8c2217d825b15aee
Onji Kim <xodks672@gmail.com>
2023-08-10 22:59:09 +0900

    [UPDATE] client repository id type

--- authorization server/src/main/java/click/porito/commons/auth2authserver/domains/oauth2_client/repository/ClientRepository.java
+++ authorization server/src/main/java/click/porito/commons/auth2authserver/domains/oauth2_client/repository/ClientRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface ClientRepository extends JpaRepository<Client,Long> {
+public interface ClientRepository extends JpaRepository<Client,String> {
     Optional<Client> findByClientId(String clientId);
 }
